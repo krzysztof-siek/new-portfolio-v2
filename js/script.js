fb1300699347aa59d93443651fd81adfc1c425c3
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-11-09 20:57:29 +0100

    add responsive navigation and add logic to it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('działa')
+const navButton = document.querySelector('.nav-button');
+const navLinks = document.querySelector('.nav-links');
+
+
+toggleNavLinks = () => {
+	navButton.classList.toggle('open')
+	navLinks.classList.toggle('open')
+}
+
+navButton.addEventListener('click', toggleNavLinks)
