c56df86131d929e6b110cd70e3f5521b7d8aaad8
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-11-10 21:05:31 +0100

    add basic structure of about section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const navButton = document.querySelector('.nav-button');
 const navLinks = document.querySelector('.nav-links');
 
-
 toggleNavLinks = () => {
 	navButton.classList.toggle('open')
 	navLinks.classList.toggle('open')
