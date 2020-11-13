f1b22074ee95129aa8f1a0c69eb55bec4f80ede8
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-11-13 21:02:06 +0100

    projects section in progress

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ toggleNavLinks = () => {
 }
 
 navButton.addEventListener('click', toggleNavLinks)
+
+
+// swiper 
+var swiper = new Swiper('.swiper-container', {
+	effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
